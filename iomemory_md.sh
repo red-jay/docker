709adccb165fe3780e6453e7bda922861885741d
RJ Bergeron <rbergero@gmail.com>
2017-06-24 03:42:53 -0400

    clean out more bashisms with the power of sed

--- iomemory_md.sh
+++ iomemory_md.sh
@@ -10,10 +10,10 @@ case "${1}" in prereqs) prereqs ; exit 0 ;; esac
 for x in $(cat /proc/cmdline) ; do
   case "${x}" in
     iomemory_md=*)
-      md_args=${x#iomemory_md=}
-      md_name=${md_args#:*}
-      md_dev=${md_args%${md_name}:}
-      md_dev=$(echo "${md_dev}" | sed 's/,/ /g' )
+      md_args=$( echo "${x}" | sed 's/^iomemory_md=//' )
+      md_name=$( echo "${md_args}" | sed 's/:.*//' )
+      md_dev=$( echo "${md_args}" | sed "s/${md_name}://" )
+      md_dev=$( echo "${md_dev}" | sed 's/,/ /g' )
       # shellcheck disable=SC2086
       mdadm --assemble "/dev/md/${md_name}" ${md_dev}
     ;;
