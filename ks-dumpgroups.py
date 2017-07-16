781f2b65111465a20223857e26e32f88990ab142
RJ Bergeron <rbergero@gmail.com>
2017-07-16 19:33:54 -0400

    teach dumppkgs, dumpgroups to not _care_ about includes

--- ks-dumpgroups.py
+++ ks-dumpgroups.py
@@ -2,7 +2,7 @@
 
 from pykickstart.parser import *
 from pykickstart.version import makeVersion
-ksparser = KickstartParser(makeVersion('RHEL7'))
+ksparser = KickstartParser(makeVersion('RHEL7'),followIncludes=False)
 ksparser.readKickstart("ks.cfg")
 ksgroups = ksparser.handler.packages.groupList
 ksgroupnames = [group.name for group in ksgroups]
--- ks-dumppkgs.py
+++ ks-dumppkgs.py
@@ -5,7 +5,7 @@ pp = pprint.PrettyPrinter(indent=4)
 
 from pykickstart.parser import *
 from pykickstart.version import makeVersion
-ksparser = KickstartParser(makeVersion('RHEL7'))
+ksparser = KickstartParser(makeVersion('RHEL7'),followIncludes=False)
 ksparser.readKickstart("ks.cfg")
 kspkgs = ksparser.handler.packages.packageList
 for n in kspkgs:
