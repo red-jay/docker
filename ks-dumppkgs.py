1ddc039d18b290a27876b8393e50040235ef32e9
RJ Bergeron <rbergero@gmail.com>
2017-07-16 18:03:41 -0400

    add missing dump packages script

---
+++ ks-dumppkgs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+from pykickstart.parser import *
+from pykickstart.version import makeVersion
+ksparser = KickstartParser(makeVersion('RHEL7'))
+ksparser.readKickstart("ks.cfg")
+kspkgs = ksparser.handler.packages.packageList
+for n in kspkgs:
+  print n
