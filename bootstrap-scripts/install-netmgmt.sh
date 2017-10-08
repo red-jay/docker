aeeb8cd8edb5828fdaf15abec4966b13602f704d
RJ Bergeron <rbergero@gmail.com>
2017-10-07 20:14:31 -0400

    include site code in netmgmt install

--- bootstrap-scripts/install-netmgmt.sh
+++ bootstrap-scripts/install-netmgmt.sh
@@ -11,7 +11,7 @@ case "${1}" in
     ;;
 esac
 
-virt-install --location http://192.168.128.129/bootstrap/centos7/ --name netmgmt --memory 1280 --os-variant rhel7.4 --disk size=12 --graphics none --network bridge=netmgmt --network bridge=vmm --extra-args="console=ttyS0,115200n8 ks=http://192.168.128.129/bootstrap/ks/netmgmt.ks site=${1} rd.neednet=1 ip=eth1:dhcp biosdevname=0 net.ifnames=0" --noautoconsole --noreboot --wait -1
+virt-install --location http://192.168.128.129/bootstrap/centos7/ --name netmgmt.${1} --memory 1280 --os-variant rhel7.4 --disk size=12 --graphics none --network bridge=netmgmt --network bridge=vmm --extra-args="console=ttyS0,115200n8 ks=http://192.168.128.129/bootstrap/ks/netmgmt.ks site=${1} rd.neednet=1 ip=eth1:dhcp biosdevname=0 net.ifnames=0" --noautoconsole --noreboot --wait -1
 
-virsh setmem netmgmt 393216 --config
-virsh setmaxmem netmgmt 786432 --config
+virsh setmem netmgmt.${1} 393216 --config
+virsh setmaxmem netmgmt.${1} 786432 --config
