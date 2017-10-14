15b968542171c04fdeaa9b79f17d8fe0d596ef26
RJ Bergeron <rbergero@gmail.com>
2017-10-14 00:34:42 -0400

    update install-netmgmt-cd to handle xen or qemu

--- bootstrap-scripts/install-netmgmt-cd.sh
+++ bootstrap-scripts/install-netmgmt-cd.sh
@@ -2,7 +2,18 @@
 
 set -e
 
-virt-install --cdrom /var/lib/libvirt/images/netmgmt-inst.iso --name netmgmt.${1} --memory 768 --os-variant rhel7.4 --disk size=12 --graphics none --network bridge=netmgmt --network bridge=vmm --noautoconsole --noreboot --wait -1
+virsh_uri=$(virsh uri)
+
+case "${virsh_uri}" in
+  xen*)
+    os_variant="rhel7"
+    ;;
+  qemu*)
+    os_variant="rhel7.4"
+    ;;
+esac
+
+virt-install --cdrom /var/lib/libvirt/images/netmgmt-inst.iso --name netmgmt.${1} --memory 768 --os-variant ${os_variant} --disk size=12 --graphics none --network bridge=netmgmt --network bridge=vmm --noautoconsole --noreboot --wait -1 -v
 
 virsh setmem netmgmt.${1} 393216 --config
 virsh setmaxmem netmgmt.${1} 786432 --config
