6040f9d77fb6b97959b65ada10df068ebd217971
RJ Bergeron <rbergero@gmail.com>
2017-12-29 01:18:10 -0500

    oh let's wire in some hardware IDs this should be fun

---
+++ envvars.tf
@@ -0,0 +1,9 @@
+variable "nickel_hwid" {
+  type = "map"
+  default = {}
+}
+
+variable "kallax_hwid" {
+  type = "map"
+  default = {}
+}
--- hosts.tf
+++ hosts.tf
@@ -1,5 +1,15 @@
 locals {
   host-mac-net-mapping = "${map(
-    "lr-kallax-sw", "${map("class","netmgmt","hwaddr","a021.b7af.2abd")}"
+    "lr-kallax-sw", "${map("class","netmgmt",   "hwaddr","${lookup(var.kallax_hwid,"ether")}")}",
+    "nickel-hw",    "${map("class","hypervisor","hwaddr","${lookup(var.nickel_hwid,"ether")}")}",
+    "nickel",       "${map("class","hypervisor","hwaddr",random_id.nickel_mac.hex)}",
   )}"
+}
+
+resource "random_id" "nickel_mac" {
+  keepers = {
+    sysmac = "${lookup(var.nickel_hwid,"ether")}"
+  }
+  byte_length = 3
+  prefix = "5e267e"
 }
