5038c6cc7290812469fcb0c62b3246f9e92c49c9
RJ Bergeron <rbergero@gmail.com>
2018-01-18 21:07:46 -0500

    update xen install for /mnt/sysimage

--- xen-install.sh
+++ xen-install.sh
@@ -5,7 +5,7 @@ set -eux
 set -o pipefail
 
 chroot_ag() {
-  chroot /mnt/target env LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get "${@}"
+  chroot /mnt/sysimage env LC_ALL=C DEBIAN_FRONTEND=noninteractive apt-get "${@}"
 }
 
 # install
@@ -34,12 +34,12 @@ xen_cmdline="dom0_max_vcpus=${cpumax} dom0_mem=${memmax}"
 read vendor < /sys/class/dmi/id/chassis_vendor
 
 # figure out serial console by asking grub what _it_ does
-grub_term=$(augtool -r /mnt/target print /files/etc/default/grub/GRUB_TERMINAL)
+grub_term=$(augtool -r /mnt/sysimage print /files/etc/default/grub/GRUB_TERMINAL)
 grub_term="${grub_term#* = }"
 
 case "${grub_term}" in
   *serial*)
-    grub_sercmd=$(augtool -r /mnt/target print /files/etc/default/grub/GRUB_SERIAL_COMMAND)
+    grub_sercmd=$(augtool -r /mnt/sysimage print /files/etc/default/grub/GRUB_SERIAL_COMMAND)
     grub_sercmd="${grub_sercmd#* = }"
     for param in ${grub_sercmd} ; do
       case "${param}" in
@@ -64,7 +64,7 @@ case "${plat}" in
 esac
 
 # adapt the grub linux boot options for the grub linux/xen boot options
-lx_def_cmdline=$(augtool -r /mnt/target print /files/etc/default/grub/GRUB_CMDLINE_LINUX_DEFAULT)
+lx_def_cmdline=$(augtool -r /mnt/sysimage print /files/etc/default/grub/GRUB_CMDLINE_LINUX_DEFAULT)
 lx_def_cmdline="${lx_def_cmdline#* = }"
 lx_def_cmdline="${lx_def_cmdline:3:-3}"
 lx_xe_def_cmdline='console=hvc0 earlyprintk=xenboot'
@@ -83,10 +83,10 @@ done
 xen_cmdline=\"\\\""${xen_cmdline}"\\\"\"
 lx_xe_def_cmdline=\"\\\""${lx_xe_def_cmdline}"\\\"\"
 
-augtool -r /mnt/target -s set /files/etc/default/grub/XEN_OVERRIDE_GRUB_DEFAULT 1
-augtool -r /mnt/target -s set /files/etc/default/grub/GRUB_CMDLINE_XEN "${xen_cmdline}"
-augtool -r /mnt/target -s set /files/etc/default/grub/GRUB_CMDLINE_LINUX_XEN_REPLACE_DEFAULT "${lx_xe_def_cmdline}"
+augtool -r /mnt/sysimage -s set /files/etc/default/grub/XEN_OVERRIDE_GRUB_DEFAULT 1
+augtool -r /mnt/sysimage -s set /files/etc/default/grub/GRUB_CMDLINE_XEN "${xen_cmdline}"
+augtool -r /mnt/sysimage -s set /files/etc/default/grub/GRUB_CMDLINE_LINUX_XEN_REPLACE_DEFAULT "${lx_xe_def_cmdline}"
 
-chroot /mnt/target sh -c "cd /etc/grub.d && ln -s 20_linux_xen 01_linux_xen"
+chroot /mnt/sysimage sh -c "cd /etc/grub.d && ln -s 20_linux_xen 01_linux_xen"
 
-chroot /mnt/target grub-mkconfig -o /boot/grub/grub.cfg
+chroot /mnt/sysimage grub-mkconfig -o /boot/grub/grub.cfg
