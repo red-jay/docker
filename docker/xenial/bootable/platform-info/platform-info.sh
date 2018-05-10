f92a6dbb2ef7eb10b9a570690d6884765d07c529
RJ Bergeron <rbergeron@palantir.com>
2018-05-10 18:51:21 -0400

    hp here, at least, is using chassis identity. fine.

--- docker/xenial/bootable/platform-info/platform-info.sh
+++ docker/xenial/bootable/platform-info/platform-info.sh
@@ -2,14 +2,18 @@
 
 pi_dir=/run/platform-info
 
-if [ -f /sys/class/dmi/id/board_vendor ] ; then
-  bv_words="${pi_dir}/board_vendor_words"
-  mkdir -p "${bv_words}"
-  read -r board_vendor < /sys/class/dmi/id/board_vendor
-  for word in ${board_vendor} ; do
-    case "${word}" in
-      "."|".."|"/"|"\\"|"\n") : ;;
-      *) touch "${bv_words}/${word,,}" ;;
-    esac
-  done
-fi
+for f in /sys/class/dmi/id/board_vendor /sys/class/dmi/id/chassis_vendor ; do
+  words=''
+  if [ -f "${f}" ] ; then
+    b="${f##*/}"
+    worddir="${pi_dir}/${b}_words"
+    mkdir -p "${worddir}"
+    read -r words < "${f}"
+    for word in ${words} ; do
+      case "${word}" in
+        "."|".."|"/"|"\\"|"\n") : ;;
+        *) touch "${worddir}/${word,,}" ;;
+      esac
+    done
+  fi
+done
