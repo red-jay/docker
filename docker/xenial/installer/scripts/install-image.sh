606d77906dad496fd00a023f7b743f82de8e7032
RJ Bergeron <rbergeron@palantir.com>
2018-05-10 19:12:54 -0400

    correctly scribble over machine-id so systemd doesn't just flail

--- docker/xenial/installer/scripts/install-image.sh
+++ docker/xenial/installer/scripts/install-image.sh
@@ -11,4 +11,8 @@ mount -o bind /sys/ /mnt/sysimage/sys/
 chroot /mnt/sysimage /bin/run-parts /scripts/grub-config
 chroot /mnt/sysimage /bin/run-parts /scripts/dracut-config
 
-systemd-machine-id-setup --root=/mnt/sysimage
+: >   /mnt/sysimage/etc/machine-id
+rm -f /mnt/sysimage/var/lib/dbus/machine-id
+
+rm -f /mnt/sysimage/etc/resolv.conf
+ln -s /run/resolvconf/resolv.conf /mnt/sysimage/etc/resolv.conf
